f1fee637df48d9309025348ef0ede550a4b89e07
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2020-09-24 16:40:25 +0900

    add AlphaPolynom

--- polynom.c
+++ polynom.c
@@ -192,3 +192,113 @@ int ModPolynom(polynom *dest, polynom *src)
 
   return 0;
 }
+
+size_t MaxDimensionAlpha(polynom *pp)
+{
+  size_t max = 0;
+
+  for (size_t l = 0; l < pp->dim; l++) {
+    if (~pp->coeff[l]) {
+      max = l;
+    }
+  }
+
+  return max;
+}
+
+int xAlphaPolynom(polynom *pp, int k)
+{
+  for (size_t l = 0; l < MaxDimensionAlpha(pp) + 1; l++) {
+    pp->coeff[l] += k;
+  }
+
+  return 0;
+}
+
+int Alpha2Polynom(polynom *pp)
+{
+  for (size_t l = 0; l < pp->dim; l++) {
+    pp->coeff[l] = alpha2int(pp->coeff[l]);
+  }
+  return 0;
+}
+
+int ShiftAlphaPolynom(polynom *pp, size_t n)
+{
+  polynom tmp;
+
+  if (pp->dim < MaxDimensionAlpha(pp) + n) { return -1; }
+
+  InitPolynom2(&tmp, pp->dim);
+  SetAlphaPolynom(&tmp, 0);
+  for (size_t l = 0; l < MaxDimensionAlpha(pp) + 1; l++) {
+    tmp.coeff[l+n] = pp->coeff[l];
+  }
+  CopyPolynom(pp, &tmp);
+  ClearPolynom(&tmp);
+
+  return 0;
+}
+
+int ModAlphaPolynom(polynom *dest, polynom *src)
+{
+  polynom tmp;
+
+  if (MaxDimensionAlpha(src) == 0 && src->coeff[0] == ~0) { return -1; }
+
+  InitPolynom2(&tmp, dest->dim);
+  while (MaxDimensionAlpha(src) <= MaxDimension(dest)) {
+    CopyPolynom(&tmp, src);
+    xAlphaPolynom(&tmp, int2alpha(dest->coeff[MaxDimension(dest)]) - src->coeff[(MaxDimensionAlpha(src))]);
+    ShiftAlphaPolynom(&tmp, MaxDimension(dest) - MaxDimensionAlpha(src));
+    Alpha2Polynom(&tmp);
+    PrintPolynom(&tmp);
+    (void) getchar();
+    XorPolynom(dest, &tmp);
+  }
+  ClearPolynom(&tmp);
+
+  return 0;
+}
+
+int SetAlphaPolynom(polynom *pp, size_t len, ...)
+{
+  va_list ap;
+  
+  if (pp->dim < len) { return -1; }
+
+  memset(pp->coeff, ~0, pp->dim * sizeof(int));
+
+  va_start(ap, len);
+  for (size_t l = 0; l < len; l++) {
+    pp->coeff[l] = va_arg(ap, int);
+  }
+  va_end(ap);
+
+  return 0;
+}
+
+int SetAlphaPolynom2(polynom *pp, size_t len, int *v)
+{
+  if (pp->dim < len) { return -1; }
+
+  memset(pp->coeff, ~0, pp->dim * sizeof(int));
+
+  for (size_t l = 0; l < len; l++) {
+    pp->coeff[l] = v[l];
+  }
+
+  return 0;
+}
+
+int PrintAlphaPolynom(polynom *pp)
+{
+  for (size_t l = MaxDimension(pp) + 1; l > 0; l--) {
+    if (~pp->coeff[l-1]) {
+      printf("+α^%dx^%u ", pp->coeff[l-1], l-1);
+    }
+  }
+  putchar('\n');
+
+  return 0;
+}
