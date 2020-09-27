d90aef9bef4c675a0831d77f818d41488b7816f4
KusaReMKN <kusaremkn@gmail.com>
2020-09-27 22:56:49 +0900

    upd help
    help の表示を関数にしてみたよ

--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <locale.h>
+#include <ctype.h>
 
 #define _GNU_SOURCE
 #include <getopt.h>
@@ -8,28 +9,35 @@
 #define MAJOR 0
 #define MINOR 0
 
+static const char Level[] = "lmqh";
+
 static struct option longopts[] = {
-  {"help",    no_argument, NULL, 'h'},
-  {"version", no_argument, NULL, 'v'},
+  {"help",    no_argument,       NULL, 'h'},
+  {"version", no_argument,       NULL, 'v'},
+  {"level",   required_argument, NULL, 'l'},
+  {"Version", required_argument, NULL, 'V'},
   {0, 0, 0, 0}
 };
+
+void help(const char *);
 
 int main(int argc, char *argv[])
 {
   int opt;
 
-  while (~(opt = getopt_long(argc, argv, "vh", longopts, NULL))) {
+  while (~(opt = getopt_long(argc, argv, "vhl:V:", longopts, NULL))) {
     switch (opt) {
     case 'v':
       printf("%s Version %1d.%02d\n", argv[0], MAJOR, MINOR);
       exit(EXIT_SUCCESS);
       /* NOTREACHED */
     case 'h':
-      printf("Usage:\n    %s [options] [string]\n\n", argv[0]);
-      printf("    -h, --help\t\tshow list of command-line options\n");
-      printf("    -v, --version\t\tshow version\n");
+      help(argv[0]);
       exit(EXIT_SUCCESS);
       /* NOTREACHED */
+    case 'l':
+    case 'V':
+      printf("not implemented\n");
     case '?':
       exit(EXIT_FAILURE);
       /* NOTREACHED */
@@ -37,4 +45,17 @@ int main(int argc, char *argv[])
   }
 
   return 0;
+}
+
+void help(const char *cmd)
+{
+  printf((
+        "Usage:\n"
+        "    %s [options] [string]\n"
+        "\n"
+        ), cmd);
+  printf((
+        "    -h, --help          Display this help messaage\n"
+        "    -v, --version       Display version info\n"
+        ));
 }
