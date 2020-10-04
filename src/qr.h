9c69fd71336f95a29a6767d23f2bee1bf0d9e1f9
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2020-10-04 16:05:17 +0900

    upd qr
    QR コードを作るにおいて大切なことがわからなくなったので作り直したよ

--- src/qr.h
+++ src/qr.h
@@ -2,6 +2,8 @@
 #define __QR_H__
 
 #include <stddef.h>
+#include <string.h>
+#include <malloc.h>
 
 #include <sys/types.h>
 #include <regex.h>
@@ -12,6 +14,16 @@
 #define MODE_KANJI  0x08
 #define MODE_ECI    0x07
 
-int GetMode(const char *);
+typedef struct _qrdata_t {
+  size_t bitlen;
+  unsigned char *buf;
+  size_t buflen;
+  struct _qrdata_t *next;
+} qrdata;
+
+int CheckMode(const char *, int);
+
+int InitQRData(qrdata *);
+int ClearQRData(qrdata *);
 
 #endif /* __QR_H__ */
